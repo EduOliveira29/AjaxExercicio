fc1133756f027bd2eaada10d0082d7b1d3e096f9
EduOliveira29 <144156214+EduOliveira29@users.noreply.github.com>
2025-01-13 13:16:55 -0300

    utilizando o try/catch

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
-    const endpoint = "https://api.github.com/users/EduOliveira29"
-    const nameElement = document.querySelector('#name');
-    const usernameElement = document.querySelector('#username');
-    const avatarElement = document.querySelector('#avatar');
-    const reposElement = document.querySelector('#repository');
-    const followersElement = document.querySelector('#followers');
-    const followingElement = document.querySelector('#following');
-    const linkElement = document.querySelector('#link');
+    try {
+        const endpoint = "https://api.github.com/users/EduOliveira29"
+        const nameElement = document.querySelector('#name');
+        const usernameElement = document.querySelector('#username');
+        const avatarElement = document.querySelector('#avatar');
+        const reposElement = document.querySelector('#repository');
+        const followersElement = document.querySelector('#followers');
+        const followingElement = document.querySelector('#following');
+        const linkElement = document.querySelector('#link');
 
     $.ajax (endpoint).done(function (resposta) {
         console.log (resposta)
@@ -18,4 +19,8 @@ $(document).ready(function () {
         followingElement.innerText = resposta.following
         linkElement.href = resposta.html_url
     })
+    .catch(error => console.error('Erro ao buscar dados do GitHub', error));
+    } catch (err) {
+        console.error('Erro no script principal:', err);
+    }
 })
